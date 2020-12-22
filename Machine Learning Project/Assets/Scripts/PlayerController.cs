a3ea06d40190196c1778d50aea15db4bb36330e5
MathiasK <mathiasmundtkofod@gmail.com>
2020-12-22 19:02:42 +0100

    Track and driving mechanics done
    Driving mechanics are ready, but can use more tweaking
    
    The track is more or less done. It could be better, but i think it's fine for now
    
    TODO:
    -Implement speed-o-meter
    -Implement method based control so an AI can control the car

--- Machine Learning Project/Assets/Scripts/PlayerController.cs
+++ Machine Learning Project/Assets/Scripts/PlayerController.cs
@@ -6,78 +6,102 @@ using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
+    public static PlayerController MyPlayerController;
+
+    private void Awake()
+    {
+        if (MyPlayerController == null) MyPlayerController = this;
+    }
     
     [Header("Car Status")]
-    public float turnValue = 0;
-    public float speed;
-
-    public float horizontalInput;
-    public float forwardInput;
+    public float turnValue = 0;     //Current rotation value to turn
+    public float speed;             //Current speed
     
-    [Header("SpeedValues")]
-    public int maxSpeed;
-    public int turnSpeed;
-    public float accelerationSpeed;
-    public float brakeSpeed;
-    public float driftingMultiplier;
+    [Header("Speed Values")]
+    public int maxSpeed;            //Max speed that vehicle can achieve
+    public float brakeSpeed;        //Deaccelerate value when breaking
+    public float accelerationSpeed; //Acceleration value
+    public float deaccelerationMultiplier;
 
+    [Header("Turn Values")]
+    public float maxTurnRate;       //Max turnValue when turning
+    public float minTurnRate;       //Min turnValue when turning
+    public float turnRateResetSpeed;//Determines how long it will take from turning to driving straight forward
+    private float _maxTurnRateActual;    //Current maxTurnRate
+    
+    [Header("Other")]
+    public float turnRate;
     public bool breaking;
+
+
+
 
     // Update is called once per frame
     void Update()
     {
+        /*
+         * Test. Trying to determine turn-rate by how fast the vehicle is moving
+         */
+
+        _maxTurnRateActual = map(Math.Abs(speed), 0, maxSpeed, maxTurnRate, 0);
+        if (_maxTurnRateActual < 2) _maxTurnRateActual = 2;
         
         
-        /*horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
-
-        transform.Translate(Vector3.forward * Time.deltaTime * maxSpeed * forwardInput);
-        transform.Rotate(Vector3.up,turnSpeed*horizontalInput*Time.deltaTime);*/
-
-
-        if (Input.GetKey(KeyCode.RightArrow))
+        
+        
+        //Turning
+        if (Input.GetKey(KeyCode.RightArrow) && Math.Abs(speed) > 0.1)
         {
-            turnValue += turnSpeed * Time.deltaTime;
-        } else if (Input.GetKey(KeyCode.LeftArrow))
+            turnValue += turnRate * Time.deltaTime;
+            
+        } else if (Input.GetKey(KeyCode.LeftArrow) && Math.Abs(speed)  > 0.1)
         {
-            turnValue -= turnSpeed * Time.deltaTime;
+            turnValue -= turnRate * Time.deltaTime;
         }
         else
         {
-            turnValue = 0;
+            if (turnValue < 0) turnValue += turnRate * turnRateResetSpeed * Time.deltaTime;
+            if (turnValue > 0) turnValue -= turnRate * turnRateResetSpeed * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            speed += accelerationSpeed * Time.deltaTime;
-        } else if (Input.GetKey(KeyCode.DownArrow))
-        {
-            speed -= accelerationSpeed * Time.deltaTime;
-        }else if (Input.GetKey(KeyCode.Space))
+        //Breaking and acceleration
+        if (Input.GetKey(KeyCode.Space))
         {
             if (speed > 0) speed -= brakeSpeed * Time.deltaTime;
             if (speed < 0) speed += brakeSpeed * Time.deltaTime;
             breaking = true;
+        }else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            speed += accelerationSpeed * Time.deltaTime;
+            
+        } else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            speed -= accelerationSpeed * Time.deltaTime;
         }
         else
         {
-            if(speed > 0) speed -= accelerationSpeed * Time.deltaTime;
-            if(speed < 0) speed += accelerationSpeed * Time.deltaTime;
+            if(speed > 0) speed -= accelerationSpeed * deaccelerationMultiplier * Time.deltaTime;
+            if(speed < 0) speed += accelerationSpeed * deaccelerationMultiplier * Time.deltaTime;
             breaking = false;
         }
         
-        //Restraints
+        //Enforcing restraints
         if (speed > maxSpeed) speed = maxSpeed;
         if (speed < -maxSpeed) speed = -maxSpeed;
-        if (turnValue > turnSpeed) turnValue = turnSpeed;
-        if (turnValue < -turnSpeed) turnValue = -turnSpeed;
-        
+        if (Math.Abs(speed) < 0.1) speed = 0;
+        if (turnValue > _maxTurnRateActual) turnValue = _maxTurnRateActual;
+        if (turnValue < -_maxTurnRateActual) turnValue = -_maxTurnRateActual;
+
+
+        //Moving vehicle
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Rotate(Vector3.up,turnSpeed*turnValue*Time.deltaTime);
+        transform.Rotate(Vector3.up,turnRate*turnValue*Time.deltaTime);
+        
+        
 
         //TODO - Implement the rest of the car controls
-        
-        
+
+        return;
         RaycastHit hit;
         // Does the ray intersect any objects excluding the player layer
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
@@ -91,5 +115,10 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Did not Hit");
         }
 
+    }
+    
+    float map(float value, float fromMin, float fromMax, float toMin, float toMax)
+    {
+        return toMin + (value-fromMin)*(toMax-toMin)/(fromMax-fromMin);
     }
 }
---
+++ Machine Learning Project/Assets/Scripts/WheelMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelMovement : MonoBehaviour
+{
+    [Header("Wheels")] 
+    public GameObject leftFrontWheel;
+    public GameObject rightFrontWheel;
+
+    public float turnMultiplier;
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float turnValue = PlayerController.MyPlayerController.turnValue;
+        
+        leftFrontWheel.transform.localRotation = Quaternion.Euler(0,turnValue*turnMultiplier,0);
+        rightFrontWheel.transform.localRotation = Quaternion.Euler(0,turnValue*turnMultiplier,0);
+    }
+}
